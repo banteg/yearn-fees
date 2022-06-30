c5eeb260a4c80ba308dc452841237ba695e1ba5b
banteg <4562643+banteg@users.noreply.github.com>
2022-07-01 01:29:44 +0400

    feat: add compare

--- yearn_fees/types.py
+++ yearn_fees/types.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel
 from rich import box
 from rich.console import Console
 from rich.table import Table
+from eth_utils.humanize import humanize_seconds
 
 
 class Fees(BaseModel):
@@ -22,8 +23,8 @@ class Fees(BaseModel):
     def total_fee(self):
         return self.management_fee + self.performance_fee + self.strategist_fee
 
-    def as_table(self, decimals):
-        table = Table(box=box.SIMPLE)
+    def as_table(self, decimals, title=None):
+        table = Table(title=title, box=box.SIMPLE)
         table.add_column("name")
         table.add_column("value", justify="right")
         table.add_column("% gain", justify="right")
@@ -34,6 +35,33 @@ class Fees(BaseModel):
                 format(Decimal(getattr(self, name)) / 10**decimals, f",.{decimals}f"),
                 format(getattr(self, name) / self.gain, ".2%") if name != "gain" else "--",
             )
+        if self.duration:
+            table.add_row("duration", format(self.duration, ",d"), humanize_seconds(self.duration))
+
+        console = Console()
+        console.print(table)
+
+    def compare(self, other, decimals):
+        assert isinstance(other, Fees), "can only compare to Fees"
+        table = Table(box=box.SIMPLE)
+        table.add_column("name")
+        table.add_column("left", justify="right")
+        table.add_column("right", justify="right")
+        table.add_column("test")
+
+        for name in ["management_fee", "performance_fee", "strategist_fee", "total_fee", "gain"]:
+            table.add_row(
+                name,
+                format(Decimal(getattr(self, name)) / 10**decimals, f",.{decimals}f"),
+                format(Decimal(getattr(other, name)) / 10**decimals, f",.{decimals}f"),
+                f"[green]✔︎" if getattr(self, name) == getattr(other, name) else "[red]✘",
+            )
+        table.add_row(
+            "duration",
+            format(self.duration, ",d"),
+            format(other.duration, ",d"),
+            f"[green]✔︎" if self.duration == other.duration else "[red]✘",
+        )
 
         console = Console()
         console.print(table)
